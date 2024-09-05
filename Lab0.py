20a3498e4f7c5dead5230b1c6af4b39f686a1405
mwijdoogen <mwijdoogen@loyola.edu>
2024-09-05 13:38:14 -0400

    Second Commit

--- Lab0.py
+++ Lab0.py
@@ -5,5 +5,10 @@ print("on the soccer team")
 print("Computer Science Major")
 print("Favorite food is spaghetti")
 
-
+print("Cody")
+print("18 years old")
+print("From New Jersey")
+print("Will be on the cross country team next year")
+print("Informations systems major")
+print("Favourite food is chicken")
 
